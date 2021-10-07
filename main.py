53b4ee05a3948901af5ac1819c4d9b879e2ddb2a
Toni-d-e-v <toni.dumancic3@gmail.com>
2021-10-07 18:58:41 +0200

    ip

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import socket
 hostname = socket.gethostname()   
 IPAddr = socket.gethostbyname(hostname)   
 from node import server
-
 args = sys.argv
 port_arg = args[1]
 seed_arg = args[2]
 # P2P storage
 # P2P network
-
 # send data over p2p network
 
+import requests
+my_ip = requests.get('http://ip.42.pl/raw').text
 
 def start_node_seed(port):
     IP = socket.gethostbyname(hostname) # get ip address of hostname
     node = server("127.0.0.1", port)
-    print("Our IP:",IPAddr)
+    print("Our IP:",my_ip)
     node_list = []
     time.sleep(1)
     # Do not forget to start your node!
@@ -33,23 +33,25 @@ def get_strings_from_list_with_dict(list_with_dict):
     return list_of_strings
 
 def start_node(port):
-    IP = socket.gethostbyname(hostname) # get ip address of hostname
-    node = server("127.0.0.1", port)
-    print("Our IP:",IPAddr)
-    node_list = []
-    time.sleep(1)
-    # Do not forget to start your node!
-    node.start()
-    # Connect with another node, otherwise you do not create any network!
-    # Example of sending a message to the nodes (dict).
-    # awaited while loop
-    if seed_arg != "True":
-        node.connect_with_node("redstone.crypto.ba", 12345) # connect to seed node
-        node.send_to_nodes({"message": "init1","register": "True", "port": 12345, "host": "redstone.crypto.ba"})
+        IP = socket.gethostbyname(hostname) # get ip address of hostname
+        node = server("127.0.0.1", port)
+        print("Our IP:",my_ip   )
+        node_list = []
+        time.sleep(1)
+        # Do not forget to start your node!
+        node.start()
+        # Connect with another node, otherwise you do not create any network!
+        # Example of sending a message to the nodes (dict).
+        # awaited while loop
+  
+        node.connect_with_node("80.240.21.187", 12345) # connect to seed node
+        node.send_to_nodes({"message": "init1","register": "True", "port": port, "host": my_ip})
         node.send_to_nodes({"message": "init"})
         time.sleep(1)
         for i in range(len(server.node_list)):
             node.connect_with_node(server.node_list[i]["host"], server.node_list[i]["port"])
+        print("Connected to seed node")
+        print(" STARTING GUI ")
         import PySimpleGUI as sg
 
         sg.theme('DarkAmber')   # Add a touch of color
@@ -65,10 +67,7 @@ def start_node(port):
         window = sg.Window('RedChat P2P', layout)
         # Event Loop to process "events" and get the "values" of the inputs
         while True:
-
-
             event, values = window.read()
-
             if event == sg.WIN_CLOSED or event == 'Exit': # if user closes window or clicks cancel
                 break
                 sys.exit()
@@ -78,7 +77,6 @@ def start_node(port):
             }
             server.messages.append(message)
             send_msg_chat(values[0],node)
-
         window.close()
 
 
